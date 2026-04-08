bc16e2ac38d3bf77b85d9c0329b3ed5cacb4a35a
gauravjha850 <gauravjha850@gmail.com>
2026-04-08 11:06:50 +0530

    Time: 306 ms (23.06%) | Memory: 27.2 MB (77.22%) - LeetSync

--- 4213-merge-adjacent-equal-elements/merge-adjacent-equal-elements.py
+++ 4213-merge-adjacent-equal-elements/merge-adjacent-equal-elements.py
@@ -1,13 +1,12 @@
 class Solution(object):
-    def mergeAdjacent(self, arr):
+    def mergeAdjacent(self, nums):
         stack=[]
+        for num in nums :
+            stack.append(num)
 
-        for x in arr:
-            while stack and stack[-1]==x:
-                stack_top=stack.pop()
-                x+= stack_top
-            stack.append(x)
+            while len(stack)>= 2 and stack[-1]==stack[-2]:
+                sum_stack= stack.pop()+stack.pop()
 
+                stack.append(sum_stack)
         return stack
-            
         
