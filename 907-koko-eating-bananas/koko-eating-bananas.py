3b97c1a34acd107c8ed1d506bca1645eb4fce2a8
gauravjha850 <gauravjha850@gmail.com>
2026-04-13 10:19:43 +0530

    Time: 160 ms (68.09%) | Memory: 13.4 MB (55.23%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        low=1
+        high=max(piles)
+        while low <= high:
+
+            mid=low+(high-low)//2
+            total_hours=0
+
+            for pile in piles :
+                total_hours=total_hours+(pile+mid-1)//mid
+            if total_hours<= h :
+                high=mid-1
+            else:
+                low=mid+1
+        return low
+        
