427497925d13eb54ee292c461d7d36dbd9ebc275
gauravjha850 <gauravjha850@gmail.com>
2026-04-26 20:19:29 +0530

    Time: 3 ms (89.21%) | Memory: 12.4 MB (26.14%) - LeetSync

--- 4290-valid-elements-in-an-array/valid-elements-in-an-array.py
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.py
@@ -1,28 +1,26 @@
 class Solution(object):
     def findValidElements(self, nums):
-        n = len(nums)
-        if n == 0: return []
-        
-        is_valid = [False] * n
-        stack = []
-        left_max = float('-inf')
-        
-        for i in range(n):
-            
-            if nums[i] > left_max:
-                is_valid[i] = True
-                left_max = nums[i]
-            
-        
-            while stack and nums[i] >= nums[stack[-1]]:
+        n=len(nums)
+        if n==0:
+            return []
+        stack=[]
+        winners=set()
+        left_max=float('-inf')
+        for i in range (n):
+            current=nums[i]
+            if current > left_max :
+                winners.add(i)
+                left_max=current
+            while stack and current >= nums[stack[-1]]:
                 stack.pop()
-            
             stack.append(i)
+        winners.update(stack)
+        result =[]
+        for i in range(n):
+            if i in winners :
+                result.append(nums[i])
+        return result
+
+
         
-        
-        for idx in stack:
-            is_valid[idx] = True
-            
-        
-        return [nums[i] for i in range(n) if is_valid[i]]
         
