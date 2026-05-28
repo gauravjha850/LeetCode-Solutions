6834e8c80f3ebc61740c58322fb02343b2def8dd
gauravjha850 <gauravjha850@gmail.com>
2026-05-28 15:16:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (21.83%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def integerReplacement(self, n):
+        count=0
+        while n>1:
+            if n%2==0:
+                n=n//2
+            
+            elif n==3 or n%4==1:
+                n=n-1
+            else: 
+                n=n+1
+            count +=1
+        return  count
+            
+
+
+        
+        
