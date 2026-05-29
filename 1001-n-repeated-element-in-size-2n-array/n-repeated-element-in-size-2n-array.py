8d41567a0185acc38d38d070ce12055de535bbe3
gauravjha850 <gauravjha850@gmail.com>
2026-05-29 13:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (68.53%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def repeatedNTimes(self, nums):
+        seen =set()
+        for num in nums:
+            if num in seen :
+                return num
+            seen.add(num)
+
+
+
+
+        
