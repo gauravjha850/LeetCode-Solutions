7870c3c6cd618f67d36782bb0fd3a6de17e7c939
gauravjha850 <gauravjha850@gmail.com>
2026-06-10 04:43:34 +0530

    Time: 31 ms (52.42%) | Memory: 13.2 MB (30.11%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -1,23 +1,10 @@
+import heapq
 class Solution(object):
     def maximumProduct(self, nums):
-        max1=max2=max3=float('-inf')
-        min1=min2=float('inf')
-
-        for num in nums :
-            if num >max1:
-                max3=max2
-                max2=max1
-                max1=num
-            elif num >max2:
-                max3=max2
-                max2=num
-            elif num > max3:
-                max3=num
-            if num < min1:
-                min2=min1
-                min1=num
-            elif num<min2:
-                min2=num
-        return max(max1*max2*max3,min1*min2*max1)
-            
+        three=heapq.nlargest(3,nums)
+        two=heapq.nsmallest(2,nums)
+        option1=three[0]*three[1]*three[2]
+        option2=two[0]*two[1]*three[0]
+        return max(option1,option2)
+        
         
