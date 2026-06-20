68c48c29cf24ad5ec3a3b53017c3e12bb93bf35b
gauravjha850 <gauravjha850@gmail.com>
2026-06-20 16:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.35%) - LeetSync

---
+++ 263-ugly-number/ugly-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isUgly(self, n):
+        if n <=0:
+            return False
+        for num in [2,3,5]:
+            while n%num==0:
+                n//=num
+        return n==1
+
+
+        
